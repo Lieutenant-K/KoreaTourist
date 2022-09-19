951aad1cf774b65f9defe142ff8c4403844fe7ac
Lieutenant-K <kts4047@gmail.com>
2022-09-20 07:25:41 +0900

    build: install realm

--- KoreaTourist/Controller/BaseViewController.swift
+++ KoreaTourist/Controller/BaseViewController.swift
@@ -21,6 +21,11 @@ class BaseViewController: UIViewController {
         super.init(nibName: nil, bundle: nil)
     }
     
+    
+    deinit {
+        print("\(type(of: Self.self)) deinit")
+    }
+    
     // 사용 시 컴파일 오류 발생
     @available(*, unavailable)
     required init?(coder: NSCoder) {
