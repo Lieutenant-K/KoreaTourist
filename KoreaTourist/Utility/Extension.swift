ba66a3503fe2da3fc21aa350c0ac09b3588ab3b6
Liuetenant-K <kts4047@gmail.com>
2022-10-18 00:31:42 +0900

    fix: toast position

--- KoreaTourist/Utility/Extension.swift
+++ KoreaTourist/Utility/Extension.swift
@@ -155,13 +155,21 @@ extension UIColor {
 
 extension CGPoint {
     
-    static let markerTop = CGPoint(x: UIScreen.main.bounds.width/2, y: UIScreen.main.bounds.height/2 - 100)
+    static var markerTop: CGPoint {
+        return CGPoint(x: UIScreen.main.bounds.width/2, y: UIScreen.main.bounds.height/2 - 100)
+    }
     
-    static let centerTop = CGPoint(x: UIScreen.main.bounds.width/2, y: UIScreen.main.bounds.height/2 - 50)
+    static var centerTop: CGPoint {
+        CGPoint(x: UIScreen.main.bounds.width/2, y: UIScreen.main.bounds.height/2 - 50)
+    }
     
-    static let buttonTop = CGPoint(x: UIScreen.main.bounds.width/2, y: UIScreen.main.bounds.height*3/4 + 50)
+    static var buttonTop: CGPoint {
+        CGPoint(x: UIScreen.main.bounds.width/2, y: UIScreen.main.bounds.height*3/4 + 50)
+    }
     
-    static let top = CGPoint(x: UIScreen.main.bounds.width/2, y: 120)
+    static var top: CGPoint {
+        return CGPoint(x: UIScreen.main.bounds.width/2, y: 120)
+    }
     
 }
 
