c2fa974f57c1639f0f28002ca27c3213c5a041f5
Liuetenant-K <kts4047@gmail.com>
2022-12-14 23:57:31 +0900

    월드맵 뷰 픽스

--- KoreaTourist/Controller/WorldMapView.swift
+++ KoreaTourist/Controller/WorldMapView.swift
@@ -7,62 +7,46 @@
 
 import UIKit
 import NMapsMap
-import Then
 
 final class WorldMapView: NMFMapView {
-    
     private func configureMapView() {
-        
         setDefaultValue()
-        
         setDefaultLocation()
     }
     
-    
     private func setDefaultValue() {
-        
         logoAlign = .leftBottom
         maxZoomLevel = 18
         minZoomLevel = 8
-//        maxTilt = maxTilt
-//        contentInset = contentInset
-        setLayerGroup(NMF_LAYER_GROUP_BUILDING, isEnabled: false)
         symbolScale = 0.5
         mapType = .navi
-        
+        setLayerGroup(NMF_LAYER_GROUP_BUILDING, isEnabled: false)
         adjustInterfaceStyle(style: UITraitCollection.current.userInterfaceStyle)
-        
     }
     
     private func setDefaultLocation() {
-        
         // 서울시청 좌표
         let baseLocation = NMGLatLng(lat: 37.56661, lng: 126.97839)
         
         let defaultCameraPosition = NMFCameraPosition(baseLocation, zoom: minZoomLevel, tilt: 0, heading: 0)
         moveCamera(NMFCameraUpdate(position: defaultCameraPosition))
-        
-        
     }
     
     override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
-        
         let style = UITraitCollection.current.userInterfaceStyle
         
         adjustInterfaceStyle(style: style)
-        
     }
 
     
     // MARK: - Initializer
-    
     override init(frame: CGRect) {
         super.init(frame: frame)
         configureMapView()
     }
     
+    @available(*, unavailable)
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
 }
