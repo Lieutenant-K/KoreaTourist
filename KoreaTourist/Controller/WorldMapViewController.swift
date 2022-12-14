56ad490135236ea3b26879ff09c837c71d6d4b18
Liuetenant-K <kts4047@gmail.com>
2022-12-14 23:50:55 +0900

    월드맵 뷰컨 픽스

--- KoreaTourist/Controller/WorldMapViewController.swift
+++ KoreaTourist/Controller/WorldMapViewController.swift
@@ -10,11 +10,10 @@ import RealmSwift
 import NMapsMap
 
 final class WorldMapViewController: BaseViewController {
-    
     let worldMapView = WorldMapView()
-    
     var placeList: [PlaceMarker] = []
     
+    // MARK: - LifeCycle
     override func loadView() {
         view = worldMapView
     }
@@ -23,15 +22,34 @@ final class WorldMapViewController: BaseViewController {
         super.viewDidLoad()
         fetchPlace()
         setPlaceMarker()
-        
     }
-    
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         moveWorldMapCamera()
     }
     
+    override func configureNavigationItem() {
+        title = "월드맵"
+        navigationItem.largeTitleDisplayMode = .never
+        
+        let appear = UINavigationBarAppearance()
+        appear.configureWithTransparentBackground()
+        appear.backgroundEffect = .init(style: .regular)
+        
+        navigationItem.standardAppearance = appear
+        navigationItem.scrollEdgeAppearance = appear
+    }
+    
+    // MARK: User Interface Change
+    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        placeList.forEach { $0.updateMarkerAppearnce() }
+    }
+
+}
+
+// MARK: Helper Function
+extension WorldMapViewController {
     private func moveWorldMapCamera() {
         let points = placeList.map { $0.position }
         
@@ -46,18 +64,15 @@ final class WorldMapViewController: BaseViewController {
         }
     }
      
-    
-    func fetchPlace() {
-        
+    private func fetchPlace() {
         placeList = realm.fetchPlaces(type: CommonPlaceInfo.self)
             .where { $0.discoverDate != nil }
             .map { PlaceMarker(place: $0) }
-        
     }
     
     private func setPlaceMarker() {
-        
-        let handler: NMFOverlayTouchHandler = { [weak self] in
+        let handler: NMFOverlayTouchHandler = {
+            [weak self] in
             if let marker = $0 as? PlaceMarker {
                 let vc = PlaceInfoViewController(place: marker.placeInfo)
                 let navi = UINavigationController(rootViewController: vc)
@@ -68,36 +83,9 @@ final class WorldMapViewController: BaseViewController {
         }
         
         placeList.forEach {
-            
             $0.mapView = worldMapView
             $0.captionMinZoom = 12
             $0.touchHandler = handler
-            
         }
-        
     }
-    
-    override func configureNavigationItem() {
-        
-        title = "월드맵"
-        
-        navigationItem.largeTitleDisplayMode = .never
-        
-        let appear = UINavigationBarAppearance()
-        appear.configureWithTransparentBackground()
-        appear.backgroundEffect = .init(style: .regular)
-        
-        navigationItem.standardAppearance = appear
-        navigationItem.scrollEdgeAppearance = appear
-        
-    }
-    
-    // MARK: User Interface Change
-    
-    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
-
-        placeList.forEach { $0.updateMarkerAppearnce() }
-        
-    }
-
 }
