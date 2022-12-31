c173184854b83e1d0b5104df906a99130f98983a
Liuetenant-K <kts4047@gmail.com>
2022-12-31 23:57:49 +0900

    API Manager 코드 정리

--- KoreaTourist/APIManager/APIManager.swift
+++ KoreaTourist/APIManager/APIManager.swift
@@ -7,37 +7,24 @@
 
 import Foundation
 import Alamofire
-import SwiftyJSON
 import XMLCoder
-
 
 class APIManager {
     
     static let shared = APIManager()
     
     func requestAreaCode(completionHandler: @escaping ([AreaCode]) -> ()) {
-        
-        let requestURL = BaseURL.service(.areaCode).url
-        
-        requestData(url: requestURL) { data in
-            
+        requestData(router: .areaCode) { data in
             if let result = try? JSONDecoder().decode(Result<AreaCode>.self, from: data) {
-                
                 let codeList = result.response.body.items.item
                 
                 completionHandler(codeList)
-                
             }
-            
         }
-        
     }
     
     func requestNearPlace(pos: Circle, failureHandler: @escaping (FailureReason) -> (), completionHandler: @escaping (_ placeList: [CommonPlaceInfo]) -> Void ) {
-    
-        let requestURL = BaseURL.service(.location(pos)).url
-        
-        requestData(url: requestURL) { result in
+        requestData(router: .location(pos)) { result in
             
             if let dataList = try? JSONDecoder().decode(Result<CommonPlaceInfo>.self, from: result) {
                 
@@ -50,42 +37,27 @@ class APIManager {
                 failureHandler(.apiError(error))
                 
             } else {
-                
                 failureHandler(.noData)
-                
             }
             
         }
     }
     
     func requestPlaceIntro(contentId: Int, completionHandler: @escaping (_ data: Intro) -> Void) {
-        print(#function)
-        let requestURL = BaseURL.service(.commonInfo(contentId)).url
-        
-        requestData(url: requestURL) { data in
-            
+        requestData(router: .commonInfo(contentId)) { data in
             if let result = try? JSONDecoder().decode(Result<Intro>.self, from: data), let info = result.response.body.items.item.first {
                 
                 completionHandler(info)
-                
             }
-            
         }
-        
-        
     }
     
     func requestDetailPlaceInfo<T:Codable> (contentId: Int, contentType: ContentType, completionHandler: @escaping (_ data: T) -> Void) {
-        
-        let requestURL = BaseURL.service(.typeInfo(contentId, contentType)).url
-        
-        requestData(url: requestURL) { data in
+        requestData(router: .typeInfo(contentId, contentType)) { data in
             
             if let result = try? JSONDecoder().decode(Result<T>.self, from: data), let info = result.response.body.items.item.first {
                 
-//                print(info)
                 completionHandler(info)
-                
             }
             
         }
@@ -94,18 +66,11 @@ class APIManager {
     }
     
     func requestExtraPlaceInfo<T:Codable> (contentId: Int, contentType: ContentType, completionHandler: @escaping (_ data: [T]) -> Void) {
-        
-        let requestURL = BaseURL.service(.extraInfo(contentId, contentType)).url
-        
-        requestData(url: requestURL) { data in
-            
+        requestData(router: .extraInfo(contentId, contentType)) { data in
             if let result = try? JSONDecoder().decode(Result<T>.self, from: data) {
-                
                 let info = result.response.body.items.item
-                
-//                print(info)
+            
                 completionHandler(info)
-                
             }
             
         }
@@ -114,47 +79,54 @@ class APIManager {
     }
     
     func requestDetailImages (contentId: Int, completionHandler: @escaping (_ data: [PlaceImage]) -> Void) {
-        
-        let requestURL = BaseURL.service(.detailImage(contentId)).url
-        
-        requestData(url: requestURL) { data in
-            
+        requestData(router: .detailImage(contentId)) { data in
             if let result = try? JSONDecoder().decode(Result<PlaceImage>.self, from: data) {
-                
                 let info = result.response.body.items.item
                 
-//                print(info)
                 completionHandler(info)
-                
             }
-            
         }
-        
-        
     }
     
-    func requestData(url: String, completionHandler: @escaping (Data) -> Void ) {
-        
-        AF.request(url).validate(statusCode: 200...500).responseData { response in
+    func requestData(router: Router, completionHandler: @escaping (Data) -> Void ) {
+        AF.request(router).responseData { response in
             switch response.result {
-            case .success(let data):
-
+            case let .success(data):
                 print("request 성공 😁😁😁😁")
-
-                if let data = try? XMLDecoder().decode(ServiceResponse.self, from: data) {
-                    // 에러 메시지 처리
-                }
                 completionHandler(data)
-                
-            case .failure(let error):
+            case let .failure(error):
                 MapViewController.progressHUD.dismiss(animated: true)
                 print(error)
             }
         }
-        
     }
     
     private init() {}
-    
-    
+}
+
+protocol NetworkManager {}
+
+extension NetworkManager {
+    func requestData<T: Codable>(router: Router, completionHandler: @escaping ([T]) -> Void ) {
+        AF.request(router).responseData { response in
+            switch response.result {
+            case let .success(data):
+                print("request 성공 😁😁😁😁")
+                
+                if let result = try? JSONDecoder().decode(Result<T>.self, from: data) {
+                    let item = result.response.body.items.item
+                    
+                    completionHandler(item)
+                } else if let message = try? XMLDecoder().decode(ServiceResponse.self, from: data) {
+                    print(message.cmmMsgHeader.errMsg)
+                } else {
+                    print("no Data")
+                }
+            case let .failure(error):
+                MapViewController.progressHUD.dismiss(animated: true)
+                
+                print(error)
+            }
+        }
+    }
 }
