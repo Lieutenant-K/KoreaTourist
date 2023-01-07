44769c13103d8f75f2619ec1d7705cda84a66ee4
Liuetenant-K <kts4047@gmail.com>
2023-01-07 23:41:53 +0900

    나의 컬렉션 화면 버그 픽스

--- KoreaTourist/Controller/CollectionViewController.swift
+++ KoreaTourist/Controller/CollectionViewController.swift
@@ -26,6 +26,7 @@ class CollectionViewController: BaseViewController {
     
     var dataSource: UICollectionViewDiffableDataSource<SectionLayoutKind, Int>!
     
+    // MARK: - Life Cycle
     override func loadView() {
         view = collectionView
         collectionView.placeItemView.delegate = self
@@ -34,45 +35,13 @@ class CollectionViewController: BaseViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         configureCollectionView()
-        fetchPlaceList()
         fetchAreaList()
     }
     
-    
-    func updateSnapshot() {
-        
-        let regionCount = regionList?.count ?? 0
-        let placeCount = placeList?.count ?? 0
-        
-        var snapshot = NSDiffableDataSourceSnapshot<SectionLayoutKind, Int>()
-        snapshot.appendSections([SectionLayoutKind.region, SectionLayoutKind.place])
-        snapshot.appendItems([Int](0..<regionCount), toSection: SectionLayoutKind.region)
-        snapshot.appendItems([Int](regionCount..<regionCount + placeCount), toSection: SectionLayoutKind.place)
-        dataSource.apply(snapshot)
-        
-    }
-    
-    func fetchAreaList() {
-        
-        realm.fetchAreaCode { [weak self] codeList in
-            self?.regionList = codeList
-        }
-//        print(regionList.count)
-    }
-    
-    func fetchPlaceList() {
-        
-        placeList = realm.loadPlaces(type: CommonPlaceInfo.self).where({ $0.discoverDate != nil}).sorted(byKeyPath: "discoverDate", ascending: false)
-        
-    }
-    
     override func configureNavigationItem() {
-        
         title = "나의 컬렉션"
         
         navigationController?.navigationBar.prefersLargeTitles = true
-        
-        navigationItem.largeTitleDisplayMode = .always
         
         let closeButton = UIBarButtonItem(image: UIImage(systemName: "xmark"), style: .plain, target: self, action: #selector(touchCloseButton(_:)))
         
@@ -85,34 +54,52 @@ class CollectionViewController: BaseViewController {
         let worldMapButton = UIBarButtonItem(customView: button)
         
         navigationItem.leftBarButtonItem = closeButton
-        
         navigationItem.rightBarButtonItems = [settingButton, worldMapButton]
-        
         navigationItem.backButtonTitle = "뒤로"
+        navigationItem.largeTitleDisplayMode = .always
+    }
+}
+
+// MARK: - Helper Method
+extension CollectionViewController {
+    func updateSnapshot() {
+        let regionCount = regionList?.count ?? 0
+        let placeCount = placeList?.count ?? 0
         
+        var snapshot = NSDiffableDataSourceSnapshot<SectionLayoutKind, Int>()
+        snapshot.appendSections([SectionLayoutKind.region, SectionLayoutKind.place])
+        snapshot.appendItems([Int](0..<regionCount), toSection: SectionLayoutKind.region)
+        snapshot.appendItems([Int](regionCount..<regionCount + placeCount), toSection: SectionLayoutKind.place)
+        
+        dataSource.apply(snapshot)
     }
     
+    func fetchAreaList() {
+        realm.fetchAreaCode { [weak self] codeList in
+            self?.regionList = codeList
+            self?.fetchPlaceList()
+        }
+    }
+    
+    func fetchPlaceList() {
+        placeList = realm.loadPlaces(type: CommonPlaceInfo.self).where({ $0.discoverDate != nil}).sorted(byKeyPath: "discoverDate", ascending: false)
+    }
+}
+
+// MARK: - Action Method
+extension CollectionViewController {
     @objc func touchCloseButton(_ sender: UIBarButtonItem) {
-        
         self.dismiss(animated: true)
-        
     }
     
     @objc func touchSettingButton(_ sender: UIBarButtonItem) {
-        
         navigationController?.pushViewController(SettingViewController(), animated: true)
-        
     }
     
     @objc func touchWorldMapButton(_ sender: UIBarButtonItem) {
-        
         navigationController?.pushViewController(WorldMapViewController(), animated: true)
-        
     }
-    
-    
 }
-
 
 // MARK: - CollectionView DataSource, Delegate
 extension CollectionViewController: UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
