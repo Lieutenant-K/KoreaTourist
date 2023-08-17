e2f661d678166ab34a1633b5b835a754812cf849
Liuetenant-K <kts4047@gmail.com>
2023-08-18 00:59:49 +0900

    Coordinate 모델 추가

---
+++ KoreaTourist/Model/Coordinate.swift
@@ -0,0 +1,17 @@
+//
+//  Coordinate.swift
+//  KoreaTourist
+//
+//  Created by 김윤수 on 2023/05/23.
+//
+
+import Foundation
+
+struct Coordinate {
+    static var seoul: Coordinate {
+        return Coordinate(latitude: 37.56661, longitude: 126.97839)
+    }
+    
+    let latitude: Double
+    let longitude: Double
+}
