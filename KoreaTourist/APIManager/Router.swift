d05696a8fd79013dffe4b996d09c8484e462ca72
김윤수 <yskim@lifegoeson.kr>
2025-07-06 23:18:47 +0900

    API 엔드포인트 변경

--- KoreaTourist/APIManager/Router.swift
+++ KoreaTourist/APIManager/Router.swift
@@ -1,5 +1,5 @@
 //
-//  EndPoint.swift
+//  Router.swift
 //  KakaoMap
 //
 //  Created by 김윤수 on 2022/09/13.
@@ -11,98 +11,111 @@ import Alamofire
 
 enum Router: URLRequestConvertible {
     private var baseURL: String {
-        "https://apis.data.go.kr/B551011/KorService1"
+        "https://apis.data.go.kr/B551011/KorService2"
     }
-    
+
     private var baseParameter: [String: Any] {
-        ["serviceKey":APIKey.tourAPI.key,
-         "MobileOS":"IOS",
-         "MobileApp":"Test",
-         "_type":"json"]
+        [
+            "serviceKey": APIKey.tourAPI.key,
+            "MobileOS": "IOS",
+            "MobileApp": "Test",
+            "_type": "json",
+        ]
     }
-    
+
     // MARK: - Cases
+
     case location(Circle)
     case areaCode
     case commonInfo(Int)
     case typeInfo(Int, ContentType)
     case extraInfo(Int, ContentType)
     case detailImage(Int)
-    
+
     // MARK: - Methods
+
     var method: HTTPMethod {
         return .get
     }
-    
+
     // MARK: - Paths
+
     var path: String {
         switch self {
         case .location:
-            return "/locationBasedList1"
+            return "/locationBasedList2"
         case .areaCode:
-            return "/areaCode1"
+            return "/areaCode2"
         case .commonInfo:
-            return "/detailCommon1"
+            return "/detailCommon2"
         case .typeInfo:
-            return "/detailIntro1"
+            return "/detailIntro2"
         case .extraInfo:
-            return "/detailInfo1"
+            return "/detailInfo2"
         case .detailImage:
-            return "/detailImage1"
+            return "/detailImage2"
         }
     }
-    
+
     // MARK: - Parameters
+
     var parameters: Parameters {
         switch self {
-        case .location(let circle):
-            return ["numOfRows":30,
-                    "pageNo":1,
-                    "mapX":circle.x,
-                    "mapY":circle.y,
-                    "radius":circle.radius,
-                    "listYN":"Y",
-                    "contentTypeId":12,
-                    "arrange":"E"]
+        case let .location(circle):
+            return [
+                "numOfRows": 30,
+                "pageNo": 1,
+                "mapX": circle.x,
+                "mapY": circle.y,
+                "radius": circle.radius,
+                "contentTypeId": 12,
+                "arrange": "E",
+            ]
         case .areaCode:
-            return ["numOfRows":20, "pageNo": 1]
-        case .commonInfo(let id):
-            return ["contentId": id,
-                    "overviewYN":"Y",
-                    "catcodeYN": "Y",
-                    "addrinfoYN":"Y",
-                    "defaultYN":"Y",
-                    "firstImageYN":"Y",
-                    "areacodeYN":"Y",
-                    "mapinfoYN":"Y"]
-        case .typeInfo(let id, let type):
-            return ["contentId":id, "contentTypeId":type.rawValue]
-        case .extraInfo(let id, let type):
-            return ["contentId":id, "contentTypeId":type.rawValue]
-        case .detailImage(let id):
-            return ["numOfRows":20,
-                    "pageNo":1,
-                    "contentId":id,
-                    "imageYN":"Y",
-                    "subImageYN":"Y"]
+            return ["numOfRows": 20, "pageNo": 1]
+        case let .commonInfo(id):
+            return [
+                "contentId": id,
+                // "overviewYN": "Y",
+                // "catcodeYN": "Y",
+                // "addrinfoYN": "Y",
+                // "defaultYN": "Y",
+                // "firstImageYN": "Y",
+                // "areacodeYN": "Y",
+                // "mapinfoYN": "Y",
+            ]
+        case let .typeInfo(id, type):
+            return [
+                "contentId": id,
+                "contentTypeId": type.rawValue,
+            ]
+        case let .extraInfo(id, type):
+            return ["contentId": id, "contentTypeId": type.rawValue]
+        case let .detailImage(id):
+            return [
+                "numOfRows": 20,
+                "pageNo": 1,
+                "contentId": id,
+                "imageYN": "Y",
+                // "subImageYN": "Y",
+            ]
         }
     }
-    
-    
+
     // MARK: - URL Request
+
     func asURLRequest() throws -> URLRequest {
-        guard let url = URL(string: self.baseURL)?.appendingPathComponent(self.path) else {
-            throw AFError.invalidURL(url: self.baseURL)
+        guard let url = URL(string: baseURL)?.appendingPathComponent(path) else {
+            throw AFError.invalidURL(url: baseURL)
         }
-        
+
         var urlRequest = URLRequest(url: url)
         urlRequest.method = method
-        
-        var param = self.baseParameter.merging(self.parameters) { left, right in
-            return left
+
+        var param = baseParameter.merging(parameters) { left, _ in
+            left
         }
-        
+
         return try URLEncoding(destination: .methodDependent, arrayEncoding: .noBrackets).encode(urlRequest, with: param)
     }
 }
-
